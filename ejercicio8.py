90aa00586a2f06eca604f25c6970ad14f267761d
hmartiljau <hmartiljau1@educacion.navarra.es>
2023-10-20 23:33:16 +0200

    ejercicio8

--- ejercicio8.py
+++ ejercicio8.py
@@ -1,4 +1,7 @@
 #Escribir un programa que pregunte por consola el precio de un producto en 
 #euros con dos decimales y muestre por pantalla el número de euros y el número 
 #de céntimos del precio introducido.
-euro=print('introduce un precio con dos decimales')
+euro=input('introduce un precio con dos decimales separados por . : ')
+formato=euro.split('.')
+print('euros: ', formato[0])
+print('centimos', formato[1])
