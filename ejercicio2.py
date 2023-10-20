d7bf43fe8696fe039cbc087405fe57642f8a2a02
hmartiljau <hmartiljau1@educacion.navarra.es>
2023-10-20 23:26:46 +0200

    ejercicio2

---
+++ ejercicio2.py
@@ -0,0 +1,19 @@
+#Escribir un programa que pregunte el nombre completo del usuario en la consola y
+#después muestre por pantalla el nombre completo del usuario tres veces,
+#una con todas las letras minúsculas, otra con todas las letras mayúsculas y
+#otra solo con la primera letra del nombre y de los apellidos en mayúscula.
+#El usuario puede introducir su nombre combinando mayúsculas y minúsculas como quiera.
+
+nombre=input('Introduce tu nombre completo: ')
+print(nombre.lower())
+print(nombre.upper())
+print(nombre.title())
+
+#i=0
+#for i in range[3]:
+#   if range==1 :
+#        print((nombre))
+#   if range==2 :
+#        print(nombre.upper())
+#    if range==3 :
+#        print(nombre.lower())
